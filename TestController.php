796e860cbe062fd175a7a5dfd45d321ba295addd
myrk95 <149483325+myrk95@users.noreply.github.com>
2023-11-07 17:50:06 +0100

    Create TestController.php

---
+++ TestController.php
@@ -0,0 +1,48 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client;
+
+class TestController
+{
+    #[Route("/test", name: "test")]
+    public function test(): Response
+    {
+        // Configura la URL base de la API de Stack Exchange y tu clave de acceso
+        $base_url = "https://api.stackexchange.com/2.3/";
+
+        // Crea un cliente Guzzle para hacer solicitudes HTTP
+        $client = new \GuzzleHttp\Client(["base_uri" => $base_url]);
+
+        // Parámetros de la solicitud para obtener una lista de preguntas
+        $params = [
+            "site" => "stackoverflow", // Sitio de Stack Exchange que deseas consultar (en este caso, Stack Overflow)
+            "order" => "desc", // Ordenar las preguntas en orden descendente
+            "sort" => "activity", // Ordenar por actividad
+            "tagged" => ",", // Filtro para incluir el tagged
+            "todate" => "1698796800	", // Filtro para to date
+            "fromdate" => "1693785600	", // Filtro para incluir el from date
+
+        ];
+
+        // Realiza la solicitud para obtener una lista de preguntas
+        $response = $client->request("GET", "questions", [
+            "query" => $params,
+          
+        ]);  dd($response );
+
+        // Decodifica la respuesta JSON
+        $data = json_decode($response->getBody());
+        dd($data );
+        // Imprime los resultados
+        foreach ($data->items as $question) {
+            echo "Título: {$question->title}\n";
+            echo "Cuerpo: {$question->body}\n\n";
+        }
+
+  
+    }
+}
